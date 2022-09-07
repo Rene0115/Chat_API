d181e92fa35f9c831de6476fc89d61ae12b57e2e
Rene0115 <enere0115@gmail.com>
2022-09-07 13:49:57 +0100

    web socket setup

--- server/app.js
+++ server/app.js
@@ -3,8 +3,14 @@ import pino from 'pino';
 
 const logger = pino();
 
-const io = new Server(3000);
+const io = new Server(3000, {
+  cors: {
+    origin: ['http://localhost:8080']
+  }
+});
 
 io.on('connection', (socket) => {
   logger.info(socket.id);
 });
+
+export default logger;
